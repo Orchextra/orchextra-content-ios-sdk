56e9f9731fa69cc9a1488d242a6780bee123545f
Alejandro Jiménez Agudo <alejandro.jimenez@gigigo.com>
2016-10-14 14:48:42 +0200

    - Refactor: MenuCoordinator to handle the logic of public method menus()

---
+++ OCM/Common/Coordinators/MenuCoordinator.swift
@@ -0,0 +1,59 @@
+//
+//  MenuCoordinator.swift
+//  OCM
+//
+//  Created by Alejandro Jiménez Agudo on 14/10/16.
+//  Copyright © 2016 Gigigo SL. All rights reserved.
+//
+
+import Foundation
+
+typealias MenusResult = ([Menu]) -> Void
+
+struct MenuCoordinator {
+	
+	let menuInteractor = MenuInteractor()
+	
+	func menus(completion: @escaping MenusResult) {
+		var sessionInteractor = SessionInteractor(
+			session: Session.shared,
+			orchextra: OrchextraWrapper()
+		)
+		
+		if sessionInteractor.hasSession() {
+			self.loadMenus(completion: completion)
+			
+		} else {
+			sessionInteractor.loadSession() { result in
+				switch result {
+				
+				case .success:
+					self.loadMenus(completion: completion)
+					
+				case .error(let error):
+					LogWarn(error)
+				}
+			}
+		}
+	}
+	
+	
+	// MARK: - Private Helpers
+	
+	private func loadMenus(completion: @escaping MenusResult) {
+		self.menuInteractor.loadMenus { result in
+			switch result {
+			case .success(let menus):
+				completion(menus)
+			
+			case .empty:
+				completion([])
+				
+			case .error(let message):
+				LogInfo("ERROR: \(message)")
+				completion([])
+			}
+		}
+	}
+	
+}
