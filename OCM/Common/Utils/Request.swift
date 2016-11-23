84f0e8b434b5de99930e465f3e5e4fbf0dede7a7
SergioLopez <sergio.lopez@gigigo.com>
2016-11-23 17:30:19 +0100

    - removed PL header

--- OCM/Common/Utils/Request.swift
+++ OCM/Common/Utils/Request.swift
@@ -33,7 +33,7 @@ extension Request {
 		return [
 			"Authorization": "Bearer \(accessToken)",
 			"Accept-Language": Locale.currentLanguage(),
-			"X-app-version": "PL"
+			"X-app-version": Config.CountryCode
 		]
 	}
 	
