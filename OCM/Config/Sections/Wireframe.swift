2ad0d709f855a0a787826d68dd5959e5c6434a39
Judith Medina <judith.medina@gigigo.com>
2016-11-28 08:56:50 +0100

    - Pushing view with the topController

--- OCM/Config/Sections/Wireframe.swift
+++ OCM/Config/Sections/Wireframe.swift
@@ -109,7 +109,11 @@ class Wireframe: NSObject {
             contentList.transition = ZoomTransitioningAnimator()
             mainContentVC.transitioningDelegate = contentList
             mainContentVC.modalPresentationStyle = .custom
-            contentList.show(mainContentVC, sender: nil)
+            
+            if let topController = UIApplication.topViewController() {
+                topController.show(mainContentVC, sender: nil)
+            }
+//            contentList.show(mainContentVC, sender: nil)
         } else {
             mainContentVC.transitioningDelegate = nil
             viewController.show(mainContentVC, sender: nil)
@@ -122,4 +126,21 @@ class OCMNavigationController: UINavigationController {
 	override var preferredStatusBarStyle: UIStatusBarStyle {
 		return .lightContent
 	}
+}
+
+extension UIApplication {
+    class func topViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
 }
