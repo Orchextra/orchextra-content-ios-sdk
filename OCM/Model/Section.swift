33cdbecf19efbe5dd148570aa15caecea7ccda6c
Carlos Vicente <carlos.vicente@gigigo.com>
2016-12-05 08:04:23 +0100

    [WO-293] - Implemented Equatable protocol in Section model object

--- OCM/Model/Section.swift
+++ OCM/Model/Section.swift
@@ -11,49 +11,65 @@ import GIGLibrary
 
 var viewCount = 0
 
-public struct Section {
-	public let name: String
-	public let slug: String
-	public let elementUrl: String
-	public let requiredAuth: String
-	
-	
-	private let actionInteractor: ActionInteractor
-	
-	init(name: String, slug: String, elementUrl: String, requiredAuth: String) {
-		self.name = name
-		self.elementUrl = elementUrl
-		self.slug = slug
-		self.requiredAuth = requiredAuth
-		
-		self.actionInteractor = ActionInteractor(dataManager: ActionDataManager(storage: Storage.shared))
-	}
-	
-	static public func parseSection(json: JSON) -> Section? {
-		guard
-			let name			= json["sectionView.text"]?.toString(),
-			let slug            = json["slug"]?.toString(),
-			let elementUrl      = json["elementUrl"]?.toString(),
-			let requiredAuth    = json["segmentation.requiredAuth"]?.toString()
-			else { LogWarn("Mandatory field not found"); return nil }
-		
-		return Section(
-			name: name,
-			slug: slug,
-			elementUrl: elementUrl,
-			requiredAuth: requiredAuth
-		)
-		
-	}
-	
-	public func openAction() -> OrchextraViewController? {
-		guard let action = self.actionInteractor.action(from: self.elementUrl) else { return nil }
-		
-		if let view = action.view() {
-			return view
-		}
-		
-		action.run()
-		return nil
-	}
+public struct Section: Equatable {
+    public let name: String
+    public let slug: String
+    public let elementUrl: String
+    public let requiredAuth: String
+    
+    
+    private let actionInteractor: ActionInteractor
+    
+    init(name: String, slug: String, elementUrl: String, requiredAuth: String) {
+        self.name = name
+        self.elementUrl = elementUrl
+        self.slug = slug
+        self.requiredAuth = requiredAuth
+        
+        self.actionInteractor = ActionInteractor(dataManager: ActionDataManager(storage: Storage.shared))
+    }
+    
+    static public func parseSection(json: JSON) -> Section? {
+        guard
+            let name			= json["sectionView.text"]?.toString(),
+            let slug            = json["slug"]?.toString(),
+            let elementUrl      = json["elementUrl"]?.toString(),
+            let requiredAuth    = json["segmentation.requiredAuth"]?.toString()
+            else { LogWarn("Mandatory field not found"); return nil }
+        
+        return Section(
+            name: name,
+            slug: slug,
+            elementUrl: elementUrl,
+            requiredAuth: requiredAuth
+        )
+        
+    }
+    
+    public func openAction() -> OrchextraViewController? {
+        guard let action = self.actionInteractor.action(from: self.elementUrl) else { return nil }
+        
+        if let view = action.view() {
+            return view
+        }
+        
+        action.run()
+        return nil
+    }
+    
+    // MARK: Equatable protocol
+    
+    public static func == (lhs: Section, rhs: Section) -> Bool {
+        let nameIsEqual = (lhs.name == rhs.name)
+        let slugIsEqual = (lhs.slug == rhs.slug)
+        let elementUrlIsEqual = (lhs.elementUrl == rhs.elementUrl)
+        let requiredAuthIsEqual = (lhs.requiredAuth == rhs.requiredAuth)
+        
+        return nameIsEqual &&
+            slugIsEqual &&
+            elementUrlIsEqual &&
+        requiredAuthIsEqual
+    }
+    
+    
 }
