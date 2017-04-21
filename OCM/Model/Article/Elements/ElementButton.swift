9a00f5b59123525684e348b8eb8500c574eabf58
Jerilyn Gonçalves <jerilyn.goncalves@gigigo.com>
2017-04-21 14:53:14 +0200

    Partial implementation of button parsing [WIP]

--- OCM/Model/Article/Elements/ElementButton.swift
+++ OCM/Model/Article/Elements/ElementButton.swift
@@ -9,23 +9,59 @@
 import UIKit
 import GIGLibrary
 
+enum ElementButtonType: String {
+    case small
+    case medium
+    case large
+}
+
 struct ElementButton: Element {
     
     var element: Element
-    var text: String?
-    var backgroundImage: Data?
-    var action: Action?
+    var buttonType: ElementButtonType
+    var elementURL: String
+
+    var title: String?
+    var titleColor: UIColor?
+    var backgroundColor: UIColor?
     
-    init(element: Element, text: String?, backgroundImage: Data?, action: Action?) {
+    var backgroundImageURL: String?
+
+    init(element: Element, buttonType: ElementButtonType, elementURL: String, title: String?, titleColor: UIColor?, backgroundColor: UIColor?) {
+        
         self.element = element
-        self.text = text
-        self.backgroundImage = backgroundImage
-        self.action = action
+        self.buttonType = buttonType
+        self.elementURL = elementURL
+        self.title = title
+        self.titleColor = titleColor
+        self.backgroundColor = backgroundColor
+    }
+    
+    init(element: Element, buttonType: ElementButtonType, elementURL: String, backgroundImageURL: String?) {
+        
+        self.element = element
+        self.buttonType = buttonType
+        self.elementURL = elementURL
+        self.backgroundImageURL = backgroundImageURL
     }
     
     static func parseRender(from json: JSON, element: Element) -> Element? {
+        
         // TODO: Parse JSON and get all data for creating a button element
-        return ElementButton(element: element, text: nil, backgroundImage: nil, action: nil)
+        guard let elementURL = json["elementUrl"]?.toString(),
+            let buttonTypeLiteral = json["type"]?.toString(),
+            let buttonType = ElementButtonType(rawValue: buttonTypeLiteral) else {
+                logWarn("Error Parsing Button")
+                return nil}
+        
+        if let title = json["text"]?.toString(),
+            let titleColor = UIColor(fromHexString: json["textColor"]?.toString()),
+            let backgroundColor = UIColor(fromHexString: json["bgColor"]?.toString()) {
+            return ElementButton(element: element, buttonType: buttonType, elementURL: elementURL, title: title, titleColor: titleColor, backgroundColor: backgroundColor)
+        } else if let backgroundImageURL = json["imageUrl"]?.toString() {
+            return ElementButton(element: element, buttonType: buttonType, elementURL: elementURL, backgroundImageURL: backgroundImageURL)
+        }
+        return .none
     }
     
     func render() -> [UIView] {
@@ -33,7 +69,7 @@ struct ElementButton: Element {
         let button = UIButton(frame: CGRect.zero)
         
         // TODO: Setup button with all data
-        button.titleLabel?.text = self.text
+        button.titleLabel?.text = self.title
         
         var elementArray: [UIView] = self.element.render()
         elementArray.append(button)
