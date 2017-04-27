35626b4faf58518a849467576becc48bb37d4bbb
cmvicentehe <cmvicentehe@gmail.com>
2017-04-27 15:57:22 +0200

    - Deleted Euforia framework from OCM.h file

--- OCM/OCM.h
+++ OCM/OCM.h
@@ -15,6 +15,3 @@ FOUNDATION_EXPORT double OCMVersionNumber;
 FOUNDATION_EXPORT const unsigned char OCMVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <OCM/PublicHeader.h>
-
-
-#import <VuforiaOrchextra/VuforiaOrchextra.h>
