35a546be039b9a7fb83b18c55cbff214ad9324cb
José Estela <jose.estela@gigigo.com>
2017-07-06 18:11:41 +0200

    Add banner to videos when there isn't internet connection & change the banner design that shows in ContentList when a content needs internet

---
+++ OCM/Config/Strings.swift
@@ -0,0 +1,19 @@
+//
+//  Strings.swift
+//  OCM
+//
+//  Created by José Estela on 6/7/17.
+//  Copyright © 2017 Gigigo SL. All rights reserved.
+//
+
+import Foundation
+
+/// Use this class to customize strings that OCM need to display in some cases
+public class Strings {
+    
+    /**
+     Set the string of the alert that is showed when a content requires internet.
+    */
+    public var internetConnectionRequired: String = localize("error_no_internet")
+    
+}
