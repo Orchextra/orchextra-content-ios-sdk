bbb2c4c8346fea447ebfde5a15b807201a107141
Eduardo Parada <udelunar@gmail.com>
2017-10-03 11:17:15 +0200

    solve bug spinner

--- OCM/Common/Base/Utils/Spinable.swift
+++ OCM/Common/Base/Utils/Spinable.swift
@@ -21,22 +21,20 @@ struct Spinable {
         )
         
         self.spinner.stopAnimating()
-        self.spinner.center = view.center
+        self.spinner.center = CGPoint(x: view.center.x, y: 200)
         self.backgroundView = UIView(
             frame: CGRect(
                 x: 0,
                 y: 0,
-                width: UIScreen.main.bounds.size.width,
-                height: UIScreen.main.bounds.size.height
+                width: view.width(),
+                height: view.height()
             )
         )
         self.backgroundView.backgroundColor = UIColor.black
         self.backgroundView.alpha = 0
         
         view.addSubview(self.backgroundView)
-        view.addSubview(self.spinner)        
-        view.addSubviewWithAutolayout(self.backgroundView)
-        view.addSubviewWithAutolayout(self.spinner)
+        view.addSubview(self.spinner)
     }
     
     func showSpinner(show: Bool) {
