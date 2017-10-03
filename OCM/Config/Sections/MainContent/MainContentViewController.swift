b4fccfaac540077ee209ff4b458dfda108477b3c
José Estela <jose.estela@gigigo.com>
2017-10-03 17:35:53 +0200

    Add X icon to webviews instead of back icon

--- OCM/Config/Sections/MainContent/MainContentViewController.swift
+++ OCM/Config/Sections/MainContent/MainContentViewController.swift
@@ -241,7 +241,11 @@ class MainContentViewController: OrchextraViewController, MainContentUI, WebVCDe
         }
         
         self.initNavigationButton(button: self.shareButton, icon: UIImage.OCM.shareButtonIcon, withPreview: self.previewView != nil)
-        self.initNavigationButton(button: self.backButton, icon: UIImage.OCM.backButtonIcon, withPreview: self.previewView != nil)
+        if self.previewView == nil && self.action is ActionWebview {
+            self.initNavigationButton(button: self.backButton, icon: UIImage.OCM.closeButtonIcon, withPreview: self.previewView != nil)
+        } else {
+            self.initNavigationButton(button: self.backButton, icon: UIImage.OCM.backButtonIcon, withPreview: self.previewView != nil)
+        }
        
         if Config.contentNavigationBarStyles.type == .navigationBar {
             // Set header
@@ -293,6 +297,11 @@ class MainContentViewController: OrchextraViewController, MainContentUI, WebVCDe
                 }
                 self.scrollView.layoutIfNeeded()
             }, completion: { (_) in
+                if headerBackgroundAlpha == 1 && self.action is ActionWebview {
+                    self.backButton.setImage(UIImage.OCM.closeButtonIcon?.withRenderingMode(.alwaysTemplate), for: .normal)
+                } else {
+                    self.backButton.setImage(UIImage.OCM.backButtonIcon?.withRenderingMode(.alwaysTemplate), for: .normal)
+                }
                 if isAppearing {
                     self.setupNavigationTitle(isAppearing: isAppearing, animated: animated)
                 }
