d274b831beff4499e28b03aeab2ef867dec41cb4
José Estela <jose.estela@gigigo.com>
2017-10-10 09:25:10 +0200

    Change OCM host in demo application

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController, OCMDelegate {
         self.ocm.delegate = self
         self.ocm.analytics = self
         //let ocmHost = "https://" + InfoDictionary("OCM_HOST")
-        let ocmHost = "http://192.168.20.135:8003"
+        let ocmHost = "https://cm.orchextra.io"
         self.ocm.offlineSupport = false
         self.ocm.host = ocmHost
         self.ocm.logLevel = .none
