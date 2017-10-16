a27a943dc8b7ff5118cbec7f2094c044d5c53e58
Jerilyn Gonçalves <jerilyn.goncalves@gigigo.com>
2017-10-16 12:38:11 +0200

    Bugfix WH-1089: Add control when displaying passbook, allow display but cancel navigation on webview.

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController, OCMDelegate {
         self.ocm.delegate = self
         self.ocm.analytics = self
         //let ocmHost = "https://" + InfoDictionary("OCM_HOST")
-        let ocmHost = "https://cm.orchextra.io"
+        let ocmHost = "https://cm.q.orchextra.io"
         self.ocm.offlineSupport = true
         self.ocm.host = ocmHost
         self.ocm.logLevel = .none
@@ -63,10 +63,10 @@ class ViewController: UIViewController, OCMDelegate {
         self.addProviders()
         self.ocm.businessUnit = "it"
         
-        let orchextraHost = "https://sdk.orchextra.io"
+        let orchextraHost = "https://sdk.q.orchextra.io"
         let orchextraApiKey = "8286702045adf5a3ad816f70ecb80e4c91fbb8de"
         let orchextraApiSecret = "eab37080130215ced60eb9d5ff729049749ec205"
-        
+                
         self.ocm.orchextraHost = orchextraHost
         self.ocm.start(apiKey: orchextraApiKey, apiSecret: orchextraApiSecret) { _ in  self.ocm.loadMenus() }
         
