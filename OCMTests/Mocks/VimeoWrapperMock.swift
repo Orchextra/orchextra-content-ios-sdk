e6b4c37510d754b3d8566dd7325c9ba7b0ab3e59
José Estela <jose.estela@gigigo.com>
2017-11-07 10:48:09 +0100

    Add tests to Article Section testing the video actions

---
+++ OCMTests/Mocks/VimeoWrapperMock.swift
@@ -0,0 +1,21 @@
+//
+//  VimeoWrapperMock.swift
+//  OCMTests
+//
+//  Created by José Estela on 7/11/17.
+//  Copyright © 2017 Gigigo SL. All rights reserved.
+//
+
+import Foundation
+@testable import OCMSDK
+
+class VimeoWrapperMock: VimeoWrapperInput {
+    
+    weak var output: VimeoWrapperOutput?
+    var getVideoResult: VimeoResult?
+    
+    func getVideo(idVideo: String) {
+        guard let videoResult = self.getVideoResult else { return }
+        self.output?.getVideoDidFinish(result: videoResult)
+    }
+}
