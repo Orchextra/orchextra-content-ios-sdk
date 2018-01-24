17a9c3354c01dd5b0dffe2ee1ac34e8b050d862f
Eduardo Parada <eduardo.parada@gigigo.com>
2018-01-24 09:20:01 +0100

    Update: Update configuration

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -63,7 +63,7 @@ class ViewController: UIViewController, OCMDelegate {
         }
         self.customize()
         self.addProviders()
-        self.ocm.businessUnit = "pl"  //  "oat-it"//       // InfoDictionary("OCM_BUSINESS_UNIT")
+        self.ocm.businessUnit = InfoDictionary("OCM_BUSINESS_UNIT")  //  "oat-it"//       // "pl" 
         
         self.startOrchextra()
         
