60c170335d2ba1a49e92fa3d6c1c7566022ee37d
eduardo parada pardo <udelunar@gmail.com>
2018-02-05 17:18:21 +0100

    Update: Change environment

--- DemoApp/AppController.swift
+++ DemoApp/AppController.swift
@@ -18,7 +18,7 @@ class AppController: NSObject, SettingOutput {
     var window: UIWindow?
     
     // Attributes Orchextra
-    var orchextraHost: Environment = .staging
+    var orchextraHost: Environment = .quality
     var orchextraApiKey = InfoDictionary("ORCHEXTRA_APIKEY")
     var orchextraApiSecret = InfoDictionary("ORCHEXTRA_APISECRET")
 
