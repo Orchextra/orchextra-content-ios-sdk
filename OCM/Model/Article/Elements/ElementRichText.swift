43dc98556c30e5be0b506c3d269da34b45540a4c
Jerilyn Gonçalves <jerilyn.goncalves@gigigo.com>
2018-05-23 11:38:39 +0200

    Fix: Set bigger system font for richtexts on articles (fixes #3467)

--- OCM/Model/Article/Elements/ElementRichText.swift
+++ OCM/Model/Article/Elements/ElementRichText.swift
@@ -44,7 +44,7 @@ class ElementRichText: Element, ActionableElement, HyperlinkTextViewDelegate {
         let view = UIView(frame: CGRect.zero)
         view.backgroundColor = .white
         
-        let textView = HyperlinkTextView(htmlText: htmlText, font: nil)
+        let textView = HyperlinkTextView(htmlText: htmlText, font: UIFont.systemFont(ofSize: 12))
         textView.hyperlinkDelegate = self
         view.addSubview(textView)
         
